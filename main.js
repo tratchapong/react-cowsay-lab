44982b397f5abb4b781b01edddee8fed0c93c16b
tratchapong <tratchapong@gmail.com>
2025-05-13 17:01:51 +0700

    stater react-cowsay lab

---
+++ main.js
@@ -0,0 +1,37 @@
+
+console.log(cowsay)
+function App() {
+  const [sayObj, setSayObj] = React.useState({})
+  return (
+    <div className="app">
+
+      <h1>Select Character</h1>
+      <p>
+        <input /> <button>update message</button> 
+      </p>
+      <div>
+        <button>BUNNY</button>
+        <button>CAT</button>
+        <button>KOALA</button>
+      </div>
+      <p>
+        <button>Bigger</button>
+        <button>Smaller</button>
+      </p>
+      <hr />
+      <Cowsay />
+    </div>
+  )
+}
+
+function Cowsay(props) {
+  return(
+    <div className="cowsay">
+      <pre>{cowsay.say({text: 'Hello...'})}</pre>
+    </div>
+  )
+}
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
